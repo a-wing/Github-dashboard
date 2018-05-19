469759a81233b086eefd2299917cb94765ade764
a-wing <shiyongxin@aliyun.com>
2018-05-19 10:48:38 +0800

    Update include && version 1.1

--- github-dashboard.user.js
+++ github-dashboard.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name     				Github dashboard sidebar right
-// @author          a-wing
-// @version  				1
+// @author  				a-wing
+// @version  				1.1
 // @grant    				none
-// @include         https://github.com/*
+// @include  				https://github.com/
+// @include  				https://github.com/*dashboard*
 // ==/UserScript==
 
 document.querySelector('.dashboard-sidebar')?document.querySelector('.dashboard-sidebar').style.cssFloat = 'right':null;
